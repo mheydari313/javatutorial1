825604030a3bd54e7a9027535c2cfeee89e2d8dc
MAHDI <heydarimarziyeh313@gmail.com>
2022-09-01 17:09:03 +0430

    commit

---
+++ src/Navigator.java
@@ -0,0 +1,13 @@
+import porteghali.E016_Variables;
+
+public class Navigator {
+
+
+    public static void main(String [] args) {
+        System.out.println("Navigator");
+        E016_Variables variables = new E016_Variables();
+        variables.Start();
+
+
+    }
+}
---
+++ src/porteghali/E016_Variables.java
@@ -0,0 +1,32 @@
+package porteghali;
+
+public class E016_Variables {
+
+
+
+    public static void Start() {
+        System.out.println("Variables");
+
+        int a = 20;
+        short  b = 6;
+        long c = 100;
+        float d = 1.1123f;
+        double f = 1298;
+        System.out.println(a+b+c+d-f);
+
+        char g = 80;
+        String h ="fatemeh and marziyeh \n ghodrat";
+        h = h+b;
+        System.out.println(h);
+
+        boolean i = true;
+        System.out.println(i);
+
+
+
+    }
+
+
+
+
+}
